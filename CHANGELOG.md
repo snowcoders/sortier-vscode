0c421dd724fe8adb24856537f2be08e6915d5d86
Ricky Patel <rickyp@snowcoders.com>
2021-12-17 21:38:12 -0800

    Migrated to sortier along with new build system

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,15 @@
-# Change Log
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+- Migrated our sortier dependency from `@snowcoders/sortier` to `sortier`. If you have a local installation, please migrate.
+- Fixed running sortier manually on a file that is ignored now sorts the file (because you explicitly requested it to be sorted)
+- Now listens to configuration changes so no need to restart VS Code on configuration change
 
 ## 1.3.1
 
--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,17 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-import * as Sortier from "@snowcoders/sortier";
-import { cosmiconfigSync } from "cosmiconfig";
+import { readFileSync } from "fs";
+import { formatText } from "sortier/format-text";
+import { isIgnored } from "sortier/is-ignored";
+import { resolveOptions } from "sortier/resolve-options";
+
+type SortierFunctions = {
+  formatText: typeof formatText;
+  isIgnored: typeof isIgnored;
+  resolveOptions: typeof resolveOptions;
+};
+const extensionName = "sortier";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,145 +24,151 @@ export function activate(context: vscode.ExtensionContext) {
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand("sortier.run", () => {
     // The code you place here will be executed every time your command is executed
-    var editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      vscode.window.showInformationMessage("No open text editor");
-      return;
-    }
-
-    getSortedText(editor.document, true).then((newText) => {
-      const oldText = editor.document.getText();
-      if (newText == null || oldText === newText) {
-        return;
-      }
-
-      editor.edit((editBuilder) => {
-        const replaceRange = new vscode.Range(
-          editor.document.positionAt(0),
-          editor.document.positionAt(editor.document.getText().length)
-        );
-        editBuilder.replace(replaceRange, newText);
-      });
-    });
+    return sortDocument(vscode.window.activeTextEditor.document, false);
   });
 
   context.subscriptions.push(disposable);
 
-  const extensionName = "sortier";
-  if (vscode.workspace.getConfiguration(extensionName).get<Boolean>("onSave")) {
-    vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
-      sortDocument(document);
-    });
-  }
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
-
-function getSortedText(
-  document: vscode.TextDocument,
-  messageIfFileNotSupported: boolean = true
-): Thenable<string> {
-  return findSortier().then((sortier) => {
-    return getSortedDocumentText(
-      document,
-      messageIfFileNotSupported,
-      sortier.formatText
-    );
+  vscode.workspace.onDidChangeConfiguration((event) => {
+    loadOnSaveConfiguration(context);
   });
+  loadOnSaveConfiguration(context);
 }
 
-function sortDocument(document: vscode.TextDocument): Thenable<void> {
-  return findSortier().then((sortier) => {
-    const options = getSortierOptions(document);
-    try {
-      sortier.formatFile(document.fileName, options);
-    } catch {}
-  });
-}
-
-function findSortier(): Thenable<typeof Sortier> {
-  return vscode.workspace
-    .findFiles("package.json", "**/node_modules/**", 1)
-    .then((value) => {
-      if (value.length !== 0) {
-        let path = value[0].fsPath;
-        path = path.substring(0, path.length - "package.json".length);
-        if (path.indexOf("\\") !== -1) {
-          path = path + "node_modules\\@snowcoders\\sortier\\dist\\index.js";
-        } else {
-          path = path + "node_modules/@snowcoders/sortier/dist/index.js";
-        }
-        try {
-          let localSortier = require(path);
-          return localSortier;
-        } catch {}
-      }
-
-      console.log("Didn't find local sortier, using bundled");
-      return Sortier;
-    });
-}
-
-function getSortedDocumentText(
-  document: vscode.TextDocument,
-  messageIfFileNotSupported: boolean = true,
-  formatFunc: typeof Sortier.formatText
-): string {
-  try {
-    let options = getSortierOptions(document);
-    const fileExtension = getFileExtension(document);
-
-    const formatted = formatFunc(fileExtension, document.getText(), options);
-    if (formatted == null) {
-      throw new Error("File not supported");
-    }
-    return formatted;
-  } catch (e) {
-    if (e.message === "File not supported" && !messageIfFileNotSupported) {
+let didSaveTextDisposable: vscode.Disposable;
+function loadOnSaveConfiguration(context: vscode.ExtensionContext) {
+  const runOnSave = vscode.workspace
+    .getConfiguration(extensionName)
+    .get<Boolean>("onSave");
+  if (runOnSave) {
+    if (didSaveTextDisposable != null) {
+      // Already created, no need to recreate
       return;
     }
-    vscode.window.showErrorMessage("Sortier threw an error: " + e.message);
+
+    didSaveTextDisposable = vscode.workspace.onDidSaveTextDocument(
+      (document: vscode.TextDocument) => {
+        sortDocument(document, true);
+      }
+    );
+    context.subscriptions.push(didSaveTextDisposable);
+  } else {
+    if (didSaveTextDisposable == null) {
+      // Already disposed
+      return;
+    }
+    didSaveTextDisposable.dispose();
+    didSaveTextDisposable = null;
   }
 }
 
-function getSortierOptions(document: vscode.TextDocument) {
-  const explorer = cosmiconfigSync("sortier");
-  const result = explorer.search(document.fileName);
-  if (result == null) {
-    console.log("No valid sortier config file found. Using defaults...");
+async function sortDocument(document: vscode.TextDocument, isOnSave: boolean) {
+  const sortier = await findSortier();
+  try {
+    if (isOnSave && (await isDocumentIgnored(sortier, document))) {
+      return;
+    }
+    const oldText = document.getText();
+    const options = getResolveOptionsForDocument(sortier, document);
+    const fileExtension = getFileExtension(document);
+    const newText = sortier.formatText(fileExtension, oldText, options);
+    if (newText == null || oldText === newText) {
+      return;
+    }
+
+    const edits = new vscode.WorkspaceEdit();
+    const documentRange = fullDocumentRange(document);
+    edits.replace(document.uri, documentRange, newText);
+
+    vscode.workspace.applyEdit(edits).then(() => {
+      if (isOnSave) {
+        document.save();
+      }
+    });
+  } catch (e) {
+    return;
   }
-  let options = result == null ? {} : result.config;
+}
+
+function fullDocumentRange(document: vscode.TextDocument): vscode.Range {
+  const lastLineId = document.lineCount - 1;
+  return new vscode.Range(
+    0,
+    0,
+    lastLineId,
+    document.lineAt(lastLineId).text.length
+  );
+}
+
+function isDocumentIgnored(
+  sortier: SortierFunctions,
+  document: vscode.TextDocument
+): vscode.Thenable<boolean> {
+  const uri = document.uri || vscode.workspace.workspaceFolders?.[0].uri;
+  const relativeFilePath = vscode.workspace.asRelativePath(uri);
+
+  return vscode.workspace.findFiles(".sortierignore").then((fileMatches) => {
+    if (fileMatches.length === 0) {
+      return false;
+    }
+
+    try {
+      const ignoreFileContents = readFileSync(fileMatches[0].fsPath, "utf8");
+      return sortier.isIgnored(ignoreFileContents, relativeFilePath);
+    } catch (error: unknown) {
+      return false;
+    }
+  });
+}
+
+function getResolveOptionsForDocument(
+  sortier: SortierFunctions,
+  document: vscode.TextDocument
+) {
+  const uri = document.uri || vscode.workspace.workspaceFolders?.[0].uri;
+  const options = sortier.resolveOptions(uri.fsPath);
   return options;
 }
+
+function findSortier(): vscode.Thenable<SortierFunctions> {
+  return vscode.workspace
+    .findFiles("**/node_modules/sortier/dist/index.js")
+    .then((fileMatches) => {
+      const defaultResult = {
+        formatText,
+        isIgnored,
+        resolveOptions,
+      };
+      if (fileMatches.length === 0) {
+        return defaultResult;
+      }
+
+      const { path, scheme } = fileMatches[0];
+      return import(`${scheme}://${path}`)
+        .then((localSortier) => {
+          return localSortier as SortierFunctions;
+        })
+        .catch(() => {
+          return defaultResult;
+        });
+    });
+}
+
+const getLanguageExtension = (languageId: string) =>
+  vscode.extensions.all
+    .map((i) => <any[]>(i.packageJSON as any)?.contributes?.languages)
+    .filter((i) => i)
+    .reduce((a, b) => a.concat(b), [])
+    .filter((i) => i.id === languageId && 0 < (i.extensions?.length ?? 0))
+    .map((i) => i?.extensions?.[0])?.[0] ?? languageId;
 
 function getFileExtension(document: vscode.TextDocument) {
   // First if it's an existing document, just use the extension
   const { fileName, languageId } = document;
   if (fileName && fileName.indexOf(".") !== -1) {
-    return fileName.substr(fileName.lastIndexOf(".") + 1);
+    return fileName.substring(fileName.lastIndexOf(".") + 1);
   }
 
-  // Otherwise try and get it from the language id
-  switch (languageId) {
-    case "javascript": {
-      return "js";
-    }
-    case "javascriptreact":
-    case "jsx": {
-      return "jsx";
-    }
-    case "json":
-    case "jsonc": {
-      return "json";
-    }
-    case "typescript": {
-      return "ts";
-    }
-    case "typescriptreact": {
-      return "tsx";
-    }
-    default:
-      return languageId;
-  }
+  const extension = getLanguageExtension(languageId);
+  return extension.substring(extension.lastIndexOf(".") + 1);
 }
